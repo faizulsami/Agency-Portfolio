4aa4c34f3f268519fbeb5f600e5cc78f310c61aa
unknown <kamrulhasananik019@gmail.com>
2024-01-09 15:54:53 +0600

    all done

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -1,14 +1,18 @@
+"use client"
 import Frequently from "./Frequently";
 import Hero from "./Hero";
+import Pricing from "./Pricing";
 import Process from "./Process";
 import Services from "./Services";
 
 
 const HomePage = () => {
+
   return (
     <div className="container mx-auto p-3">
       <Hero />
       <Services/>
+      <Pricing/>
       <Process/>
       <Frequently/>
     </div>
