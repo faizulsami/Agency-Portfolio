ec200f6da67a0d674a88b3828c67c4b189ea16d4
unknown <kamrulhasananik019@gmail.com>
2024-01-15 23:50:23 +0600

    all done

--- src/app/(home)/Projects.jsx
+++ src/app/(home)/Projects.jsx
@@ -3,7 +3,46 @@ import React from 'react';
 const Projects = () => {
     return (
         <div>
-            this si 
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-10 lg:grid-cols-3'>
+                <div className='border rounded-xl '>
+                    <img className='rounded-t-xl' src="https://graphicsfamily.com/wp-content/uploads/edd/2021/12/Free-Business-Card-Design-Gold-And-Black-Colors-1180x885.jpg" alt="" />
+                    <div className='p-3'>
+                        <h3 className='text-2xl'>
+                            Card image is a traditional term for a character string.
+                        </h3>
+                        <div className='flex gap-x-32 mt-5'>
+                            <p>pricee</p>
+                            <p>1500</p>
+                        </div>
+
+                    </div>
+                </div>
+                <div className='border rounded-xl '>
+                    <img className='rounded-t-xl' src="https://graphicsfamily.com/wp-content/uploads/edd/2021/12/Free-Business-Card-Design-Gold-And-Black-Colors-1180x885.jpg" alt="" />
+                    <div className='p-3'>
+                        <h3 className='text-2xl'>
+                            Card image is a traditional term for a character string.
+                        </h3>
+                        <div className='flex gap-x-32 mt-5'>
+                            <p>pricee</p>
+                            <p>1500</p>
+                        </div>
+
+                    </div>
+                </div> <div className='border rounded-xl '>
+                    <img className='rounded-t-xl' src="https://graphicsfamily.com/wp-content/uploads/edd/2021/12/Free-Business-Card-Design-Gold-And-Black-Colors-1180x885.jpg" alt="" />
+                    <div className='p-3'>
+                        <h3 className='text-2xl'>
+                            Card image is a traditional term for a character string.
+                        </h3>
+                        <div className='flex gap-x-32 mt-5'>
+                            <p>pricee</p>
+                            <p>1500</p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
