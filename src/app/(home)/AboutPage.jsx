3da32ab0b8439ee0b46825a829b0777449a684e1
kamrulhasananik019 <kamrulhasananik019@gmail.com>
2024-02-05 15:17:19 +0600

    about

--- src/app/(home)/AboutPage.jsx
+++ src/app/(home)/AboutPage.jsx
@@ -1,50 +1,102 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Pagination, Navigation } from 'swiper/modules';
 
 const AboutPage = () => {
+    const [swiperRef, setSwiperRef] = useState(null);
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <section>
             <div className='grid grid-cols-1 md:grid-cols-2 my-24'>
                 <div className='py-5 md:pb-96'>
-                    <h2 className='text-5xl md:text-7xl '>
-                        About Us
-                    </h2>
+                    <h2 className='text-5xl md:text-7xl'>About Us</h2>
                 </div>
                 <div>
                     <h3 className='text-3xl'>
                         Our team helps businesses succeed on social media
                     </h3>
-                    <p>At our company, we believe in the power of social media to drive success.
-                        Our team helps businesses succeed on social media. Our team helps businesses succeed on social media.
-                        Our team helps businesses succeed on social media.</p>
-                    <div className='md:flex gap-4 pt-14 '>
-
-                        <div
-                            className='h-96 w-96 relative '
-                            style={{
-                                backgroundImage: "url(https://lh3.googleusercontent.com/a/ACg8ocLkmKSoo-uVVRR5LbVA-ymhVzn0gsLsvf0VbXyg5rhicy0=s288-c-no)",
-                                backgroundRepeat: 'no-repeat', backgroundSize: 'cover'
+                    <p>
+                        At our company, we believe in the power of social media to drive
+                        success. Our team helps businesses succeed on social media. Our team
+                        helps businesses succeed on social media. Our team helps businesses
+                        succeed on social media.
+                    </p>
+                    <div className='my-10'>
+                        <Swiper
+                            onSwiper={setSwiperRef}
+                            slidesPerView={isMobile ? 1 : 2} // Adjust slidesPerView based on device size
+                            centeredSlides={true}
+                            spaceBetween={50}
+                            
+                            pagination={{
+                                type: 'fraction',
                             }}
+                            navigation={true}
+                            modules={[Pagination, Navigation]}
+                            className='mySwiper'
                         >
-                            <div className='absolute w-full h-full grid content-end text-center   text-white opacity-0 hover:opacity-100 transition-opacity duration-300'>
-                                    
-                                        <h3 className='text-5xl'>Anik</h3>
-                                        <p>Co-founder</p>
-                            </div>
-                        </div>
+                            <SwiperSlide>   <div className='my-10 h-96 w-96 relative '
+                                style={{
+                                    backgroundImage: "url(https://lh3.googleusercontent.com/a/ACg8ocLkmKSoo-uVVRR5LbVA-ymhVzn0gsLsvf0VbXyg5rhicy0=s288-c-no)",
+                                    backgroundRepeat: 'no-repeat', backgroundSize: 'cover'
+                                }}
+                            >
+                                <div className='absolute w-full h-full grid content-end text-center   text-white opacity-0 hover:opacity-100 transition-opacity duration-300'>
 
-                        
-                        <div
-                            className='h-96 w-96 relative'
-                            style={{
-                                backgroundImage: "url(https://lh3.googleusercontent.com/a/ACg8ocLkmKSoo-uVVRR5LbVA-ymhVzn0gsLsvf0VbXyg5rhicy0=s288-c-no)",
-                                backgroundRepeat: 'no-repeat', backgroundSize: 'cover'
-                            }}
-                        >
-                            <div className='absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 text-white opacity-0 hover:opacity-100 transition-opacity duration-300'>
-                                <h3>Anik</h3>
-                                <p>Co-founder</p>
-                            </div>
-                        </div>
+                                    <h3 className='text-5xl'>Anik</h3>
+                                    <p>Co-founder</p>
+                                </div>
+                            </div></SwiperSlide>  <SwiperSlide>   <div className='my-10 h-96 w-96 relative '
+                                style={{
+                                    backgroundImage: "url(https://lh3.googleusercontent.com/a/ACg8ocLkmKSoo-uVVRR5LbVA-ymhVzn0gsLsvf0VbXyg5rhicy0=s288-c-no)",
+                                    backgroundRepeat: 'no-repeat', backgroundSize: 'cover'
+                                }}
+                            >
+                                <div className='absolute w-full h-full grid content-end text-center   text-white opacity-0 hover:opacity-100 transition-opacity duration-300'>
+
+                                    <h3 className='text-5xl'>Anik</h3>
+                                    <p>Co-founder</p>
+                                </div>
+                            </div></SwiperSlide>  <SwiperSlide>   <div className='my-10 h-96 w-96 relative '
+                                style={{
+                                    backgroundImage: "url(https://lh3.googleusercontent.com/a/ACg8ocLkmKSoo-uVVRR5LbVA-ymhVzn0gsLsvf0VbXyg5rhicy0=s288-c-no)",
+                                    backgroundRepeat: 'no-repeat', backgroundSize: 'cover'
+                                }}
+                            >
+                                <div className='absolute w-full h-full grid content-end text-center   text-white opacity-0 hover:opacity-100 transition-opacity duration-300'>
+
+                                    <h3 className='text-5xl'>Anik</h3>
+                                    <p>Co-founder</p>
+                                </div>
+                            </div></SwiperSlide>  <SwiperSlide>   <div className='my-10 h-96 w-96 relative '
+                                style={{
+                                    backgroundImage: "url(https://lh3.googleusercontent.com/a/ACg8ocLkmKSoo-uVVRR5LbVA-ymhVzn0gsLsvf0VbXyg5rhicy0=s288-c-no)",
+                                    backgroundRepeat: 'no-repeat', backgroundSize: 'cover'
+                                }}
+                            >
+                                <div className='absolute w-full h-full grid content-end text-center   text-white opacity-0 hover:opacity-100 transition-opacity duration-300'>
+
+                                    <h3 className='text-5xl'>Anik</h3>
+                                    <p>Co-founder</p>
+                                </div>
+                            </div></SwiperSlide>
+                        </Swiper>
                     </div>
                 </div>
             </div>
