02deec4a76320cfdeb21675c6b0d5cb96ab566f6
kamrulhasananik019 <kamrulhasananik019@gmail.com>
2024-04-01 02:44:07 +0600

    main done

--- src/app/(home)/AboutPage.jsx
+++ src/app/(home)/AboutPage.jsx
@@ -7,19 +7,24 @@ import { Pagination, Navigation } from 'swiper/modules';
 
 const AboutPage = () => {
     const [swiperRef, setSwiperRef] = useState(null);
-    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+    const [isMobile, setIsMobile] = useState(false); // Initialize with false
 
     useEffect(() => {
         const handleResize = () => {
-            setIsMobile(window.innerWidth <= 768);
+            setIsMobile(window.innerWidth <= 1080);
         };
 
+        // Call handleResize on initial render to set the initial state
+        handleResize();
+
+        // Add event listener for window resize
         window.addEventListener('resize', handleResize);
 
+        // Remove event listener on component unmount
         return () => {
             window.removeEventListener('resize', handleResize);
         };
-    }, []);
+    }, []); // Empty dependency array ensures the effect runs only once on mount
 
     return (
         <section>
@@ -40,10 +45,9 @@ const AboutPage = () => {
                     <div className='my-10 '>
                         <Swiper
                             onSwiper={setSwiperRef}
-                            slidesPerView={isMobile ? 1 : 2} // Adjust slidesPerView based on device size
+                            slidesPerView={isMobile ? 1 : 2}
                             centeredSlides={true}
                             spaceBetween={30}
-
                             pagination={{
                                 type: 'fraction',
                             }}
@@ -51,33 +55,22 @@ const AboutPage = () => {
                             modules={[Pagination, Navigation]}
                             className='mySwiper'
                         >
-                            <SwiperSlide>   <div className='my-10  h-96 w-96 relative '
-                                style={{
-                                    backgroundImage: "url(https://lh3.googleusercontent.com/a/ACg8ocLkmKSoo-uVVRR5LbVA-ymhVzn0gsLsvf0VbXyg5rhicy0=s288-c-no)",
-                                    backgroundRepeat: 'no-repeat', backgroundSize: 'cover'
-                                }}
-                            >
-                                <div className='absolute w-full h-full grid content-end text-center   text-white opacity-0 hover:opacity-100 transition-opacity duration-300'>
-
-                                    <h3 className='text-5xl'>Anik</h3>
-                                    <p>Co-founder</p>
+                            <SwiperSlide>
+                                <div className='my-10 h-96 w-96 relative' style={{ backgroundImage: "url(https://lh3.googleusercontent.com/a/ACg8ocLkmKSoo-uVVRR5LbVA-ymhVzn0gsLsvf0VbXyg5rhicy0=s288-c-no)", backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
+                                    <div className='absolute w-full h-full grid content-end text-center text-white opacity-0 hover:opacity-100 transition-opacity duration-300'>
+                                        <h3 className='text-5xl'>Anik</h3>
+                                        <p>Co-founder</p>
+                                    </div>
                                 </div>
-                            </div>
                             </SwiperSlide>
                             <SwiperSlide>
-                                <div className='my-10  h-96 w-96 relative '
-                                    style={{
-                                        backgroundImage: "url(https://i.ibb.co/kJC4XGT/Sami.png)",
-                                        backgroundRepeat: 'no-repeat', backgroundSize: 'cover'
-                                    }}
-                                >
-                                    <div className='absolute w-full h-full grid content-end text-center   text-white opacity-0 hover:opacity-100 transition-opacity duration-300'>
-
+                                <div className='my-10 h-96 w-96 relative' style={{ backgroundImage: "url(https://i.ibb.co/kJC4XGT/Sami.png)", backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
+                                    <div className='absolute w-full h-full grid content-end text-center text-white opacity-0 hover:opacity-100 transition-opacity duration-300'>
                                         <h3 className='text-5xl'>Sami</h3>
                                         <p>Co-founder</p>
                                     </div>
-                                </div></SwiperSlide>
-
+                                </div>
+                            </SwiperSlide>
                         </Swiper>
                     </div>
                 </div>
