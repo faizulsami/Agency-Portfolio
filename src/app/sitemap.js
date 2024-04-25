01f36a696abb477aa9770b5179087f96defb8d32
kamrulhasananik019 <kamrulhasananik019@gmail.com>
2024-04-25 22:36:50 +0600

    hfskdhafkj

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+    return [
+        {
+            url: 'https://craflex.com',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 1,
+        },
+    ]
+}
